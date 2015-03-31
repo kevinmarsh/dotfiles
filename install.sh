3458960921a8772480db640884e3cf2e6dd6b0c0
Kevin Marsh <kevinmarsh3@gmail.com>
2015-03-31 14:54:44 +0100

    install: add better spacing for printed messages

--- install.sh
+++ install.sh
@@ -11,16 +11,16 @@ dotfilesdir=dotfiles
 backupdir=$HOME/dotfiles_old
 
 
-echo "Creating $backupdir for backup of any existing dotfiles in $HOME"
+echo " - Creating $backupdir for backup of any existing dotfiles in $HOME"
 mkdir -p $backupdir
 
 for file in `git ls-tree -r master --name-only`; do
     if [[ "$file" != "README.md" && "$file" != "install.sh" ]]; then
         if [ -e $HOME/$file ]; then
-            echo "Moving $file to $backupdir/$file"
+            echo " - Moving $file to $backupdir/$file"
             mv $HOME/$file $backupdir/
         fi
-        echo "Creating symlink to $file in home directory."
+        echo " - Creating symlink to $file in home directory."
         cp --parents $file $HOME
         ln -sf $HOME/$dotfilesdir/$file $HOME/$file
     fi
@@ -30,10 +30,10 @@ done
 # Create a .gitconfig.private file if it doesn't exist
 privategit=$HOME/.gitconfig.private
 if [ ! -f $privategit ]; then
-    echo -e "\n\nSetting up $privategit"
-    echo -n "What is your email? "
+    echo -e "\n - Setting up $privategit"
+    echo -n "   - What is your email? "
     read gitemail
-    echo -n "What is your full name? "
+    echo -n "   - What is your full name? "
     read gitname
 
     echo -e "[user]\n\temail = $gitemail\n\tname = $gitname" > $privategit
@@ -43,17 +43,17 @@ fi
 # Create a .hgrc.private file if it doesn't exist
 privatehg=$HOME/.hgrc.private
 if [ ! -f $privatehg ]; then
-    echo -e "\n\nSetting up $privatehg"
+    echo -e "\n - Setting up $privatehg"
 
-    read -p "Are you using the same name and email address for mercurial as well? (y/n) "
+    read -p "   - Are you using the same name and email address for mercurial as well? (y/n) "
     if [ "$REPLY" == "y" ]
     then
         hgrcemail=$gitemail
         hgrcname=$gitname
     else
-        echo -n "What is your email? "
+        echo -n "   - What is your email? "
         read hgrcemail
-        echo -n "What is your full name? "
+        echo -n "   - What is your full name? "
         read hgrcname
     fi
 
